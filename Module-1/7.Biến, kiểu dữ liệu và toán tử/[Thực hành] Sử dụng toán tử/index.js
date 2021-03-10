77494bf908d5b7134ec9928c69018c350e6b74d3
Tddnp <79992239+saoduy399@users.noreply.github.com>
2021-03-10 13:24:13 +0700

    bai tap

---
+++ Module-1/7.Biến, kiểu dữ liệu và toán tử/[Thực hành] Sử dụng toán tử/index.js
@@ -0,0 +1,8 @@
+width=prompt("Please select a number for width:");
+hight=prompt("Please select a number for hight:");
+let area=width*hight;
+document.write("width:"+ width);
+document.write("<br/>");
+document.write("hight:"+ hight);
+document.write("<br/>");
+document.write("area="+ area);
